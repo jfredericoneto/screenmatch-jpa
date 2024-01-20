a715cfa22ab3e91e99d88ae0a9787321be7fa8a2
Neto <jfredericon@gmail.com>
2024-01-20 16:50:44 -0300

    Adicionado novas buscas por critérios diversos

--- src/main/java/dev/josefredrico/screenmatch/repository/SerieRepository.java
+++ src/main/java/dev/josefredrico/screenmatch/repository/SerieRepository.java
@@ -1,9 +1,22 @@
 package dev.josefredrico.screenmatch.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import dev.josefredrico.screenmatch.model.Categoria;
 import dev.josefredrico.screenmatch.model.Serie;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
 
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+
+    List<Serie> findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, double avaliacao);
+
+    List<Serie> findTop5ByOrderByAvaliacaoDesc();
+
+    List<Serie> findByGenero(Categoria categoria);
+
+    List<Serie> findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(int totalTemporadas, double avaliacao);
 }
